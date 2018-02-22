5772f716280cb6813a7f4901af6d10f7e971e6d0
田岡滉一朗 <taokakouichiro@taokahiroshiichirounoMacBook-Pro.local>
2018-02-22 14:07:59 +0800

    index完成

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php 
 	echo 'Hallo World';
 	var_dump(1);
+
+	$name='Shinya Hirai';
  ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ </head>
+ <body>
+ 	<h1>Hello Github!</h1>
+ </body>
+ </html>
